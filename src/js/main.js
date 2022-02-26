980a050262efeb68b23338680d90a261270964d6
Hosvell <aleks.hosvell@gmail.com>
2022-02-26 16:28:47 +0400

    main

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,17 @@
 // adaptive navber onsroll
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction()
+}
 
 function scrollFunction() {
-  if (document.body.scrollTop > 10|| document.documentElement.scrollTop > 10) {
-    document.getElementById("navbar").style.background = "rgb(0, 0, 0, 0.8)";
-    document.getElementById("navbar").style.height = "8vh";
-    document.getElementById("wave__one").classList.add("wave__one__new");
-
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.getElementById('navbar').style.background = 'rgb(0, 0, 0, 0.8)'
+    document.getElementById('navbar').style.height = '8vh'
+    document.getElementById('wave__one').classList.add('wave__one__new')
   } else {
-    document.getElementById("navbar").style.background = "";
-    document.getElementById("navbar").style.height = "12vh";
-    document.getElementById("wave__one").classList.remove("wave__one__new");
-    document.getElementById("wave__one").classList.add(".wave__one");
+    document.getElementById('navbar').style.background = ''
+    document.getElementById('navbar').style.height = '12vh'
+    document.getElementById('wave__one').classList.remove('wave__one__new')
+    document.getElementById('wave__one').classList.add('.wave__one')
   }
 }
